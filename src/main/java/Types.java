606aaaa412dc80284146e24850db30c2213c16a6
Artem Kharin <artem.kharin96@gmail.com>
2023-12-26 21:31:26 +0200

    add initial implementation

---
+++ src/main/java/Types.java
@@ -0,0 +1,12 @@
+public class Types {
+    public static void main(String[] args) {
+        System.out.println(21 > 20);
+        System.out.println(!(21 > 20));
+        System.out.println(21 <= 20);
+
+        System.out.println(true);
+        System.out.println(!true);
+        System.out.println(false);
+        System.out.println(!false);
+    }
+}
